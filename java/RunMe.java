88a1abd7c8b724ef58985177917ab6d567d48898
Georgiy Korneev <kgeorgiy@keorgiy.info>
2019-09-10 15:13:01 +0300

    data -> password

--- java/RunMe.java
+++ java/RunMe.java
@@ -16,43 +16,43 @@ import java.util.stream.Stream;
 public class RunMe {
 
     public static void main(String[] args) {
-        final byte[] data = parseArgs(args);
+        final byte[] password = parseArgs(args);
 
         System.out.println("Register keys at http://gg.gg/year2019-prog-intro-1");
 
-        key0(data.clone());
+        key0(password);
         System.out.println("The first key was low-hanging fruit, can you found others?");
         System.out.println("Try to modify some code in keyX(...) functions");
 
-        key2(data.clone());
-        key3(data.clone());
-        key4(data.clone());
-        key5(data.clone());
-        key6(data.clone());
-        key7(data.clone());
-        key8(data.clone());
-        key9(data.clone());
-        key10(data.clone());
-        key11(data.clone());
-        key12(data.clone());
-        key13(data.clone());
+        key2(password);
+        key3(password);
+        key4(password);
+        key5(password);
+        key6(password);
+        key7(password);
+        key8(password);
+        key9(password);
+        key10(password);
+        key11(password);
+        key12(password);
+        key13(password);
     }
 
-    private static void key0(final byte[] data) {
+    private static void key0(final byte[] password) {
         // The result of print(...) function depends only by explicit arguments
-        print(0, 0, data);
+        print(0, 0, password);
     }
 
 
-    private static void key1(final byte[] data) {
+    private static void key1(final byte[] password) {
         while ("1".length() == 1) {
         }
 
-        print(1, 224753495938049280L, data);
+        print(1, 224753495938049280L, password);
     }
 
 
-    private static void key2(final byte[] data) {
+    private static void key2(final byte[] password) {
         int result = 0;
         for (int i = 0; i < 300_000; i++) {
             for (int j = 0; j < 300_000; j++) {
@@ -63,11 +63,11 @@ public class RunMe {
             }
         }
 
-        print(2, 7345540958740857324L, data);
+        print(2, 7345540958740857324L, password);
     }
 
 
-    private static void key3(final byte[] data) {
+    private static void key3(final byte[] password) {
         int result = 0;
         for (int i = 0; i < 3000; i++) {
             for (int j = 0; j < 3000; j++) {
@@ -78,14 +78,14 @@ public class RunMe {
             }
         }
 
-        print(3, result, data);
+        print(3, result, password);
     }
 
 
-    private static void key4(final byte[] data) {
+    private static void key4(final byte[] password) {
         for (long i = Long.MIN_VALUE; i < Long.MAX_VALUE; i++) {
             if ((i ^ (i >> 32)) == 2348327482723738188L) {
-                print(4, i, data);
+                print(4, i, password);
             }
         }
     }
@@ -93,17 +93,17 @@ public class RunMe {
 
     private static final long PRIME = 1073741789;
 
-    private static void key5(final byte[] data) {
+    private static void key5(final byte[] password) {
         long result = 0;
         for (long i = 0; i < 1000_000_000_000_000L; i++) {
             result = (result + i + i / 2 + i / 3) % PRIME;
         }
 
-        print(5, result, data);
+        print(5, result, password);
     }
 
 
-    private static void key6(final byte[] data) {
+    private static void key6(final byte[] password) {
         /*
           \u002a\u002f\u0077\u0068\u0069\u006c\u0065\u0020\u0028\u0022\u0031\u0022
           \u002e\u006c\u0065\u006e\u0067\u0074\u0068\u0028\u0029\u0020\u003d\u003d
@@ -112,16 +112,16 @@ public class RunMe {
           \u0033\u0034\u0035\u0037\u0030\u0034\u0033\u0038\u0037\u0035\u0030\u004c
           \u003b\u002f\u002a
         */
-        print(6, result, data);
+        print(6, result, password);
     }
 
 
-    private static void key7(final byte[] data) {
+    private static void key7(final byte[] password) {
         // Count the number of occurrences of the most frequent noun at this following page:
         // https://docs.oracle.com/javase/specs/jls/se11/html/jls-4.html
         int result = 0;
         if (result != 0) {
-            print(7, result, data);
+            print(7, result, password);
         }
     }
 
@@ -129,67 +129,67 @@ public class RunMe {
     private static final String PATTERN = "Hello, World! Привет, Мир!";
     private static final int SMALL_REPEAT_COUNT = 10_000_000;
 
-    private static void key8(final byte[] data) {
+    private static void key8(final byte[] password) {
         String repeated = "";
         for (int i = 0; i < SMALL_REPEAT_COUNT; i++) {
             repeated += PATTERN;
         }
 
-        print(8, repeated.hashCode(), data);
+        print(8, repeated.hashCode(), password);
     }
 
 
     private static final long LARGE_REPEAT_SHIFT = 27;
     private static final long LARGE_REPEAT_COUNT = 1L << LARGE_REPEAT_SHIFT;
 
-    private static void key9(final byte[] data) {
+    private static void key9(final byte[] password) {
         String repeated = "";
         for (long i = 0; i < LARGE_REPEAT_COUNT; i++) {
             repeated += PATTERN;
         }
 
-        print(9, repeated.hashCode(), data);
+        print(9, repeated.hashCode(), password);
     }
 
 
-    private static void key10(final byte[] data) {
-        print(10, 4508604576084534553L, data);
+    private static void key10(final byte[] password) {
+        print(10, 4508604576084534553L, password);
     }
 
 
-    private static void key11(final byte[] data) {
+    private static void key11(final byte[] password) {
         final BigInteger year = BigInteger.valueOf(-2019);
         final BigInteger prime = BigInteger.valueOf(PRIME);
 
         long result = Stream.iterate(BigInteger.ZERO, BigInteger.ONE::add)
                 .filter(i -> year.multiply(i).add(prime).multiply(i).compareTo(BigInteger.ZERO) > 0)
-                .mapToLong(i -> i.longValue() * data[i.intValue() % data.length])
+                .mapToLong(i -> i.longValue() * password[i.intValue() % password.length])
                 .sum();
 
-        print(11, result, data);
+        print(11, result, password);
     }
 
 
     private static final long MAX_DEPTH = 10_000_000L;
 
-    private static void key12(final byte[] data) {
+    private static void key12(final byte[] password) {
         try {
-            key12(data, 0, 0);
+            key12(password, 0, 0);
         } catch (StackOverflowError soe) {
             System.err.println("Stack overflow :(");
         }
     }
 
-    private static void key12(final byte[] data, long depth, long result) {
+    private static void key12(final byte[] password, long depth, long result) {
         if (depth < MAX_DEPTH) {
-            key12(data, depth + 1, (result ^ 5487) + depth * 11);
+            key12(password, depth + 1, (result ^ 5487) + depth * 11);
         } else {
-            print(12, result, data);
+            print(12, result, password);
         }
     }
 
 
-    private static void key13(final byte[] data) {
+    private static void key13(final byte[] password) {
         final BigInteger secondsInDay = BigInteger.valueOf(60 * 60 * 24);
 
         long result = Stream.iterate(BigInteger.ZERO, BigInteger.ONE::add)
@@ -197,7 +197,7 @@ public class RunMe {
                 .collect(Collectors.reducing(BigInteger.ZERO, BigInteger::add))
                 .longValue();
 
-        print(13, result, data);
+        print(13, result, password);
     }
 
     // ---------------------------------------------------------------------------------------------------------------
@@ -205,16 +205,14 @@ public class RunMe {
     // It is not required to get all keys
     // ---------------------------------------------------------------------------------------------------------------
 
-    private static void print(final int no, long result, final byte[] data) {
+    private static void print(final int no, long result, final byte[] password) {
+        final byte[] key = password.clone();
         for (int i = 0; i < 6; i++) {
-            data[i] ^= result;
+            key[i] ^= result;
             result >>>= 8;
         }
-        print(no, data);
-    }
 
-    private static void print(final int no, final byte[] data) {
-        System.out.format("Key %d: %s%n", no, key(data));
+        System.out.format("Key %d: %s%n", no, key(key));
     }
 
     private static String key(final byte[] data) {
