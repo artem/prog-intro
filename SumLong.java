548abb9401ab50849e856b87b9f34ba3d401d891
Artem Labazov <123321artyom@gmail.com>
2019-09-16 19:10:04 +0300

    Help

--- SumLong.java
+++ SumLong.java
@@ -1,36 +1,26 @@
 public class SumLong {
     static long ans;
 
-    private static boolean isDigit(char digit) {
-        return digit >= '0' && digit <= '9';
-    }
+    // private static boolean isDigit(char digit) {
+    //     return digit >= '0' && digit <= '9';
+    // }
 
-    private static void handleNumber(long add, boolean negative) {
-        if (negative) {
-            add = -add;
-        }
-        ans += add;
-    }
+    // private static void handleNumber(long add, boolean negative) {
+    //     if (negative) {
+    //         add = -add;
+    //     }
+    //     ans += add;
+    // }
 
     public static void main(String[] args) {
         ans = 0;
         for (int i = 0; i < args.length; i++) {
-            long cur = 0;
-            boolean negative = false;
-
-            for (int j = 0; j < args[i].length(); j++) {
-                char symbol = args[i].charAt(j);
-
-                if (isDigit(symbol)) {
-                    cur = cur * 10 + Character.getNumericValue(symbol);
-                } else {
-                    handleNumber(cur, negative);
-                    cur = 0;
-                    negative = symbol == '-';
-                }
+            String[] parse = args[i].split("\\p{javaWhitespace}");
+            for (int j = 0; j < parse.length; j++) {
+                try {
+                    ans += Long.parseLong(parse[j]);
+                } catch (Exception ignore) { }
             }
-            //System.out.println(cur);
-            handleNumber(cur, negative);
         }
         System.out.println(ans);
     }
