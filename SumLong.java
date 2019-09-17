7f77fdd44facfc3dd1b485a4d74e16cf80a39e44
Artem Labazov <123321artyom@gmail.com>
2019-09-18 01:55:38 +0300

    Fuck

--- SumLong.java
+++ SumLong.java
@@ -1,25 +1,17 @@
+import java.util.Arrays;
+
 public class SumLong {
-    static long ans;
-
-    // private static boolean isDigit(char digit) {
-    //     return digit >= '0' && digit <= '9';
-    // }
-
-    // private static void handleNumber(long add, boolean negative) {
-    //     if (negative) {
-    //         add = -add;
-    //     }
-    //     ans += add;
-    // }
-
     public static void main(String[] args) {
-        ans = 0;
+        long ans = 0;
+
         for (int i = 0; i < args.length; i++) {
-            String[] parse = args[i].split("\\p{javaWhitespace}");
+            String[] parse = args[i].split("\\p{javaWhitespace}+");
+            //System.err.println(Arrays.toString(parse) + parse.length);
             for (int j = 0; j < parse.length; j++) {
-                try {
-                    ans += Long.parseLong(parse[j]);
-                } catch (Exception ignore) { }
+                if (parse[j].isEmpty()) {
+                    continue;
+                }
+                ans += Long.parseLong(parse[j]);
             }
         }
         System.out.println(ans);
