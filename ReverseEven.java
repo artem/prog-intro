d54a1553c59e6f86abd5fb33932581b009e5efc6
Artem Labazov <123321artyom@gmail.com>
2019-10-06 23:34:58 +0300

    ReverseEven: migrate to FastScanner

--- ReverseEven.java
+++ ReverseEven.java
@@ -1,8 +1,9 @@
 import java.util.*;
+import java.io.*;
 
 public class ReverseEven {
-    public static int[] stringToIntArr(String s, int[] buffer) {
-        Scanner sc = new Scanner(s);
+    public static int[] stringToIntArr(String s, int[] buffer) throws UnsupportedEncodingException, IOException {
+        FastScanner sc = new FastScanner(s);
         int size = 0;
 
         while (sc.hasNextInt()) {
@@ -12,24 +13,30 @@ public class ReverseEven {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[][] numbers = new int[1_000_000][];
-        int[] buffer = new int[1_000_000];
-        int size = 0;
+        try {
+            FastScanner sc = new FastScanner(System.in);
+            int[][] numbers = new int[1_000_000][];
+            int[] buffer = new int[1_000_000];
+            int size = 0;
 
-        while (sc.hasNextLine()) {
-            numbers[size++] = stringToIntArr(sc.nextLine(), buffer);
-        }
-
-        for (int i = size - 1; i >= 0; i--) {
-            for (int j = numbers[i].length - 1; j >= 0; j--) {
-                int cur = numbers[i][j];
-
-                if (cur % 2 == 0) {
-                    System.out.print(cur + " ");
-                }
+            while (sc.hasNextLine()) {
+                numbers[size++] = stringToIntArr(sc.nextLine(), buffer);
             }
-            System.out.println();
+
+            for (int i = size - 1; i >= 0; i--) {
+                for (int j = numbers[i].length - 1; j >= 0; j--) {
+                    int cur = numbers[i][j];
+
+                    if (cur % 2 == 0) {
+                        System.out.print(cur + " ");
+                    }
+                }
+                System.out.println();
+            }
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("Console input has unsupported encoding: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("I/O error during reading console input: " + e.getMessage());
         }
     }
 }
