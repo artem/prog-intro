27d21d330d8fc486f19a95f49ca4ebf7cd1d29d6
Artem Labazov <123321artyom@gmail.com>
2019-11-17 16:45:09 +0300

    Md2Html: Works, broken multiline

--- md2html/MDParser.java
+++ md2html/MDParser.java
@@ -1,43 +1,55 @@
 package md2html;
 
-import java.util.HashMap;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Map;
 
 public class MDParser {
-    private final StringBuilder result;
-    private final StringBuilder buffer;
     private static final Map<String, String> decorators;
     private static final Map<String, String> paragraphs;
-
-    private String paragraph;
+    private static final Map<Character, String> htmlSymbols;
 
     static {
-        decorators = new HashMap<>();
-        paragraphs = new HashMap<>();
+        decorators = Map.of("*", "em",
+                "_", "em",
+                "**", "strong",
+                "__", "strong",
+                "--", "s",
+                "`", "code");
 
-        decorators.put("*", "em");
-        decorators.put("_", "em");
-        decorators.put("**", "strong");
-        decorators.put("__", "strong");
-        decorators.put("--", "s");
-        decorators.put("`", "code");
+        //TODO intelligent auto heading
+        paragraphs = Map.of("# ", "h1",
+                "## ", "h2",
+                "### ", "h3",
+                "#### ", "h4",
+                "##### ", "h5",
+                "###### ", "h6");
 
-        paragraphs.put("# ", "h1");
-        paragraphs.put("## ", "h2");
-        //TODO
+        htmlSymbols = Map.of('<', "&lt;",
+                '>', "&gt;",
+                '&', "&amp;");
     }
+
+    private final StringBuilder result;
+    private final Deque<String> tagStack;
+    private int offset;
+    private String paragraph;
 
     public MDParser() {
         result = new StringBuilder();
-        buffer = new StringBuilder();
+        tagStack = new ArrayDeque<>();
     }
 
-    private void openTag(String tag) {
-        result.append("<").append(tag).append(">");
+    private static String symbolToHtml(char c) {
+        return htmlSymbols.getOrDefault(c, String.valueOf(c));
     }
 
-    private void closeTag(String tag) {
-        result.append("</").append(tag).append(">");
+    private StringBuilder openTag(String tag, StringBuilder res) {
+        return res.append("<").append(tag).append(">");
+    }
+
+    private StringBuilder closeTag(String tag, StringBuilder res) {
+        return res.append("</").append(tag).append(">");
     }
 
     private void openParagraph(String line) {
@@ -45,30 +57,91 @@ public class MDParser {
             return;
         }
 
+        paragraph = "p";
+
         //TODO intelligent auto heading
         for (Map.Entry<String, String> parDetect : paragraphs.entrySet()) {
             if (line.startsWith(parDetect.getKey())) {
                 String tag = parDetect.getValue();
-                openTag(tag);
                 paragraph = tag;
-                return;
+                offset += parDetect.getKey().length();
+                break;
             }
         }
+
+        openTag(paragraph, result);
     }
 
     private void closeParagraph() {
         if (paragraph != null) {
-            closeTag(paragraph);
+            closeTag(paragraph, result);
             paragraph = null;
         }
     }
+
     public void append(String line) {
+        offset = 0;
         if (line.isEmpty()) {
             closeParagraph();
             return;
         }
+        if (result.length() > 0) {
+            result.append('\n');
+        }
 
         openParagraph(line);
+        result.append(parseText(line));
+    }
 
+    private StringBuilder parseText(String line) {
+        StringBuilder buffer = new StringBuilder();
+
+        outer:
+        for (; offset < line.length(); ) {
+            char cur = line.charAt(offset);
+
+            if (cur == '\\') {
+                if (offset + 1 < line.length()) {
+                    append(line.charAt(++offset), buffer);
+                    offset++;
+                    continue;
+                }
+            }
+
+            String tag;
+            for (int j = Math.min(line.length() - offset, 2); j > 0; j--) {
+                String mdFlag = line.substring(offset, offset + j);
+                if ((tag = decorators.get(mdFlag)) != null) {
+                    offset += j;
+                    if (tagStack.size() > 0 && tagStack.peek().equals(mdFlag)) {
+                        tagStack.pop();
+                        return closeTag(tag, openTag(tag, new StringBuilder()).append(buffer));
+                    } else {
+                        tagStack.push(mdFlag);
+                        buffer.append(parseText(line));
+                    }
+                    continue outer;
+                }
+            }
+
+            append(line.charAt(offset++), buffer);
+        }
+        if (tagStack.size() > 0) {
+            buffer.insert(0, tagStack.pop());
+        }
+        return buffer;
+    }
+
+    public void append(char c, StringBuilder res) {
+        res.append(symbolToHtml(c));
+    }
+
+    @Override
+    public String toString() {
+        String res = result.toString();
+        if (paragraph != null) {
+            res += "</" + paragraph + ">";
+        }
+        return res;
     }
 }
--- md2html/Md2Html.java
+++ md2html/Md2Html.java
@@ -4,15 +4,28 @@ import java.io.*;
 
 public class Md2Html {
     public static void main(String[] args) {
+        MDParser pr = new MDParser();
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(new FileInputStream(args[0]), "utf8")
         )) {
-
+            String line;
+            while ((line = reader.readLine()) != null) {
+                pr.append(line);
+            }
         } catch (FileNotFoundException e) {
             System.out.println("Input file not found: " + e.getMessage());
             e.printStackTrace();
         } catch (IOException e) {
             System.out.println("I/O error during read: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            PrintWriter writer = new PrintWriter(args[1]);
+            String out = pr.toString();
+            writer.write(out);
+            writer.close();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
